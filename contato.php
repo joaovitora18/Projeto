5cb59312ed688a0fba0537190080606144e672e7
João Vitor de Assunção Caldeira Silva <jvacs2019@gmail.com>
2022-11-21 17:26:51 -0300

    projeto inicial

---
+++ contato.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GabiPratash</title>
+</head>
+<body>
+    <!-- Inclusão do cabeçalho -->
+    <?php include('header.html'); ?>
+
+    <!-- Começo da pagina -->
+    Conteúdo
+    <!-- Fim da página -->
+
+    <!-- Inclusão do rodapé -->
+    <?php include('footer.html'); ?>
+</body>
+</html>
